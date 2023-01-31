b3d612cb0e46684c3f1fc1ebd3525d9f9ab37bd4
David Shean <dshean@gmail.com>
2023-01-30 19:48:20 -0800

    Move plot_ar to library, minor fixes

--- asp_plot_util.py
+++ asp_plot_util.py
@@ -2,7 +2,10 @@
 
 import numpy as np
 import rasterio as rio
+from rasterio import plot
 from rasterio.windows import Window
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def symm_clim(clim):
     abs_max = np.max(np.abs(clim))
@@ -26,13 +29,18 @@ def find_common_clim(im1, im2, symm=False):
         clim = symm_clim(clim)
     return clim
 
-def read_array(fn, b=1):
+def read_array(fn, b=1, extent=False):
     ds = rio.open(fn)
     a = ds.read(b, masked=True)
     ndv = get_ndv(ds)
     #The stddev grids have nan values, separate from nodata
     ma = np.ma.fix_invalid(np.ma.masked_equal(a, ndv))
-    return ma
+    out = ma
+    #Return map extent, should clean up to avoid different return
+    if extent:
+        extent = rio.plot.plotting_extent(ds)
+        out = (ma, extent)
+    return out
 
 def get_ndv(ds):
     ndv = ds.nodatavals[0]
@@ -54,3 +62,16 @@ def get_cbar_extend(a, clim=None):
     elif a.min() < clim[0] and a.max() <= clim[1]:
         extend = 'min'
     return extend
+
+def plot_ar(im, ax, cmap='inferno', clim=None, clim_perc=(2,98), label=None, cbar=True, alpha=1):
+    if clim is None:
+        clim = get_clim(im, clim_perc)
+    m = ax.imshow(im, cmap=cmap, clim=clim, alpha=alpha, interpolation='none')
+    if cbar:
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="4%", pad="2%")
+        cb = plt.colorbar(m, cax=cax, ax=ax, extend=get_cbar_extend(im, clim))
+        cax.set_ylabel(label)
+    ax.set_facecolor("0.5")
+    ax.set_xticks([])
+    ax.set_yticks([])
