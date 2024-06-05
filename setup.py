44ce0aa169b5ff64c48a1e8807f00ed8b8350af7
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 16:33:33 -0700

    update author email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.1.0",
     description="Python library and client for plotting Ames Stereo Pipeline outputs",
     author="Ben Purinton",
-    author_email="ben.purinton@gmail.com",
+    author_email="purinton@uw.edu",
     packages=find_packages(),
     entry_points={"console_scripts": ["asp_plot=asp_plot.cli.asp_plot:main"]},
 )
