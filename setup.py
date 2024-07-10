777845d460ed29a78f6ff3d87934c4622165ebf5
Ben Purinton <ben.purinton@gmail.com>
2024-07-10 13:40:28 -0700

    Compress figures in report (#28)
    Figures are converted from PNG to JPG and compressed to 95% quality, resulting in OOM reduction in final PDF size (~20 MB to ~2 MB). The JPGs are cleaned up and removed after report generation, so only the high quality individual PNGs remain for your viewing pleasure.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="asp_plot",
-    version="0.2.0",
+    version="0.2.1",
     description="Python library and client for plotting Ames Stereo Pipeline outputs",
     author="Ben Purinton",
     author_email="purinton@uw.edu",
