991e6fc3e1c1935de1b8043ef6f5b9198daf1963
Ben Purinton <ben.purinton@gmail.com>
2024-07-17 20:58:50 -0700

    add some todos

--- asp_plot/icesat2.py
+++ asp_plot/icesat2.py
@@ -23,10 +23,27 @@ class Icesat2:
         self.dem_fn = dem_fn
         self.geojson_fn = geojson_fn
 
-    def pull_sliderule_data(self):
+    def pull_atl06_data(self, srt="land", cnf="high", ats=5, cnt=5, len=40, res=10, maxi=5, H_min_win=3, sigma_r_max=5):
         if not os.path.exists(self.geojson_fn):
             raise ValueError(f"Geojson file not found: {self.geojson_fn}\nUse this tool to make and download one:\nhttps://geojson.io/")
 
         region = sliderule.toregion(self.geojson_fn)["poly"]
 
+        # TODO: make request
+
+        # TODO: optionally save to parquet and/or csv
+
         return region
+
+    # TODO: quick n' dirty plot, maybe with ctx basemap?
+    # def plot_atl06_data(self, region):
+
+    # TODO: clean it up
+    # def clean_atl06_data(self):
+        # TODO: optionally save to parquet and/or csv
+
+    # TODO: comparison plotting
+
+    # TODO: profile plotting
+
+    
