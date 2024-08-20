9a42b7b95d12eb7b4819e922121aee3e0d0fa093
Ben Purinton <ben.purinton@gmail.com>
2024-08-19 23:44:48 -0400

    decent spec coverage

--- tests/test_altimetry.py
+++ tests/test_altimetry.py
@@ -13,6 +13,9 @@ class TestAltimetry:
         icesat = Altimetry(
             dem_fn="tests/test_data/stereo/date_time_left_right_1m-DEM.tif",
         )
+        icesat.atl06sr_filtered = gpd.read_parquet(
+            "tests/test_data/icesat_data/atl06sr_defaults_filtered.parquet"
+        )
         return icesat
 
     def test_pull_and_filter_atl06sr(self, icesat):
@@ -28,18 +31,26 @@ class TestAltimetry:
 
     def test_plot_atl06sr(self, icesat):
         try:
-            icesat.atl06sr_filtered = gpd.read_parquet(
-                "tests/test_data/icesat_data/atl06sr_defaults_filtered.parquet"
-            )
             icesat.plot_atl06sr(filtered=True)
         except Exception as e:
             pytest.fail(f"plot_atl06sr() method raised an exception: {str(e)}")
 
     def test_atl06sr_to_dem_dh(self, icesat):
         try:
-            icesat.atl06sr_filtered = gpd.read_parquet(
-                "tests/test_data/icesat_data/atl06sr_defaults_filtered.parquet"
-            )
             icesat.atl06sr_to_dem_dh()
         except Exception as e:
             pytest.fail(f"atl06sr_to_dem_dh() method raised an exception: {str(e)}")
+
+    def test_mapview_plot_atl06sr_to_dem(self, icesat):
+        try:
+            icesat.mapview_plot_atl06sr_to_dem()
+        except Exception as e:
+            pytest.fail(
+                f"mapview_plot_atl06sr_to_dem() method raised an exception: {str(e)}"
+            )
+
+    def test_histogram(self, icesat):
+        try:
+            icesat.histogram()
+        except Exception as e:
+            pytest.fail(f"histogram() method raised an exception: {str(e)}")
