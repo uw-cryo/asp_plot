3410997586d984377fa898249e9b16a64a4a642e
David Shean <dshean@users.noreply.github.com>
2025-02-08 22:16:15 -0500

    Geometry plotting refactoring and cleanup (#78)
    The existing metadata parsing and scene geometry plotting was previously ported as-is from dgtools. This PR refactors the code to be more flexible for varied XML file inputs. The focus is still on WV stereopairs, but the improvements should be a good base for increasing flexibility for other satellites and more than two scene inputs.
    
    BIE and Assymetry angles were also added to the output geometry plot to resolve #19
    
    ---------
    
    Co-authored-by: Ben Purinton <ben.purinton@gmail.com>

--- asp_plot/processing_parameters.py
+++ asp_plot/processing_parameters.py
@@ -162,7 +162,7 @@ class ProcessingParameters:
 
         if search_for_reference_dem:
             reference_dem = self.get_reference_dem(
-                pprc_log, starting_string="Using input DEM:"
+                pprc_log, starting_string="Input DEM:"
             )
 
             return processing_timestamp, stereo_params, run_time, reference_dem
@@ -171,10 +171,11 @@ class ProcessingParameters:
 
     def get_reference_dem(self, logfile, starting_string="DEM:"):
         reference_dem = ""
+        pattern = re.compile(starting_string, re.IGNORECASE)
         with open(logfile, "r") as file:
             for line in file:
-                if starting_string in line:
-                    reference_dem = line.split(starting_string)[1].strip()
+                if pattern.search(line):
+                    reference_dem = pattern.split(line)[1].strip()
         return reference_dem
 
     def get_run_time(self, logfiles):
